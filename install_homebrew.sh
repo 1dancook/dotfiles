6f875985db3ec97fa8fa72d1c71158d14800627d
Dan Cook <cook.r.dan@gmail.com>
2022-12-06 13:29:34 +0900

    update: homebrew install script

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -5,5 +5,9 @@ echo "Installing Homebrew"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# update path
+
+echo 'echo "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+
 # Update homebrew
 brew update
