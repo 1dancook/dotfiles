748114c04147c6ed607fab004f893d9a5d571e24
Dan Cook <cook.r.dan@gmail.com>
2024-10-23 15:21:55 +0900

    adjustments to render markdown

---
+++ packages/nvim-lazygit/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        sign = true,
+        position = "overlay",
+      },
+      pipe_table = {
+        style = "normal",
+      },
+    },
+  },
+}
